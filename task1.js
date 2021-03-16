944e6f742f0238601e864187d7e79d8038dce9d5
priya-2645 <77579335+priya-2645@users.noreply.github.com>
2021-03-16 17:44:59 +0530

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,1 +1,21 @@
+ var x= 3;
+ console.log(typeof(x));//number
 
+ var x="priya";
+console.log(typeof(x));//string
+
+ var x={fruit:"grapes",vegetables:"potato",beverages:"tea"};
+ console.log(typeof(x));//object
+
+ var x=3;
+ var y=4;
+ console.log(x===y);//boolean
+
+ var x;
+ console.log(typeof(x));//undefined
+
+ var x=null;
+ console.log(typeof(x));//null
+
+var x=["priya","rjpm"];
+console.log(typeof(x));//array
