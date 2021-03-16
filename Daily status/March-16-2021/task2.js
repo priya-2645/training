69abd1594fc907c4ecca20d5b69e121b3415ce0b
priya-2645 <77579335+priya-2645@users.noreply.github.com>
2021-03-16 23:38:12 +0530

    Add files via upload

--- Daily status/March-16-2021/task2.js
+++ Daily status/March-16-2021/task2.js
@@ -1,1 +1,34 @@
+var arr=[1,2,3,4];
+const x=arr.map((y)=> {
+    if(y<3)
+    return y;
+})
+console.log(x); //Map
 
+const x=arr.filter((y)=>{
+    return y<3;
+})
+console.log(x); //Filter
+const x = arr.find((y) => {
+  return y===2;
+ });
+ console.log(x); //Find
+
+ const x= arr.findIndex((y)=>{
+     return y===3;
+ });
+ console.log(x);//findIndex
+
+ const x=arr.some((y)=>{
+     return y<4;
+ })
+ console.log(x);//some
+const x=arr.every((y)=>{
+    return y<4;
+})
+console.log(x);//every
+
+const x=arr.forEach((y)=>{
+    return y>3;
+})
+console.log(x);//forEach
