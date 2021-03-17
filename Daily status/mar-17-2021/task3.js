505c19e1688a35f81dc1cdb9f291672775a7d422
priya-2645 <77579335+priya-2645@users.noreply.github.com>
2021-03-17 23:40:54 +0530

    Add files via upload

--- Daily status/mar-17-2021/task3.js
+++ Daily status/mar-17-2021/task3.js
@@ -1,1 +1,40 @@
+var x=[1,'1','Name',[1,2],true];
+var result=x.map((a)=>{
+  return(typeof(a));
+})
+console.log(result); // op:number,string,string,object,boolean
 
+//splice
+var beverages=["tea","coffee","shakes","milk","lassi"];
+beverages.splice(3,0,"juice");
+console.log(beverages); 
+//slice
+ console.log(beverages.slice(3));
+//pop
+beverages.pop();
+console.log(beverages);
+//push
+beverages.push("juice");
+console.log(beverages);
+//shift
+beverages.shift();
+console.log(beverages);
+//unshift
+beverages.unshift("juice");
+console.log(beverages);
+//delete
+delete beverages[0];
+console.log(beverages);
+//sort
+beverages.sort();
+console.log(beverages);
+//reverse
+console.log(beverages.reverse());
+//tostring
+console.log(beverages.toString());
+//join
+console.log(beverages.join("+"));
+//numeric sort
+var x=[10,50,100,90,70];
+x.sort(function(a, b){return a - b});
+console.log(x);
